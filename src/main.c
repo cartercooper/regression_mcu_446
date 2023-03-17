0d6561e7872afe94988e3c50948cc4e2899eca68
Carter Cooper <carterjccooper@gmail.com>
2023-03-17 15:43:00 -0700

    cleaned up for submission

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
   * @author  Carter Cooper
   * @version V1.0
   * @date    27-January-2023
-  * @brief   Default main function.
+  * @brief   Polynomial Regression.
   ******************************************************************************
 */
 
@@ -45,6 +45,7 @@ int main(void)
 	uint32_t start_time, end_time;
 	float rmse;
 
+	//Enable clock cycle register and reset, then store the start of clock cycles.
 	DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
 	DWT->CYCCNT = 0;
 
@@ -54,14 +55,13 @@ int main(void)
 
 	end_time = DWT->CYCCNT;
 
+	//calculate elapsed clock cycles
 	uint32_t elapsed_clocks = end_time - start_time;
 
+
+	//"useless" statements, used to analyze memory during debugging
 	elapsed_clocks;
 	rmse;
-
-//	printf("POLYNOMIAL REGRESSION: DEGREE %d\n", POLY_DEGREE);
-//	printf("CLOCK CYCLES ELAPSED: %lu\n", end_time - start_time);
-//	printf("RMSE: %.2f\n", rmse);
 
 	while(1);
 }
@@ -71,13 +71,16 @@ float polynomial_regression_train_and_test(float data[SAMPLE_SIZE][DATASET_FEATU
 	// Extract features and labels from input matrix
 	float **x = malloc(sample_size * sizeof(float *));
 	float *y = malloc(sample_size * sizeof(float));
+
 	for (int i = 0; i < sample_size; i++)
 	{
 		x[i] = malloc((degree + 1) * sizeof(float));
+
 		for (int j = 0; j <= degree; j++)
 		{
 			x[i][j] = pow(data[i][0], j);
 		}
+
 		y[i] = data[i][feature_size - 1];
 	}
 
@@ -86,40 +89,49 @@ float polynomial_regression_train_and_test(float data[SAMPLE_SIZE][DATASET_FEATU
 	float **x_transpose_x = malloc((degree + 1) * sizeof(float *));
 	float *x_transpose_y = malloc((degree + 1) * sizeof(float));
 	float *temp = malloc((degree + 1) * sizeof(float));
+
 	for (int i = 0; i <= degree; i++)
 	{
 		x_transpose[i] = malloc(sample_size * sizeof(float));
 		x_transpose_x[i] = malloc((degree + 1) * sizeof(float));
+
 		for (int j = 0; j < sample_size; j++)
 		{
 			x_transpose[i][j] = x[j][i];
 		}
+
 		for (int j = 0; j <= degree; j++)
 		{
 			x_transpose_x[i][j] = 0;
+
 			for (int k = 0; k < sample_size; k++)
 			{
 			  x_transpose_x[i][j] += x_transpose[i][k] * x[k][j];
 			}
 		}
+
 		x_transpose_y[i] = 0;
+
 		for (int j = 0; j < sample_size; j++)
 		{
-		x_transpose_y[i] += x_transpose[i][j] * y[j];
+			x_transpose_y[i] += x_transpose[i][j] * y[j];
 		}
 	}
 
 	// Solve the system of equations to get coefficients
 	float *coefficients = malloc((degree + 1) * sizeof(float));
+
 	for (int i = 0; i <= degree; i++)
 	{
 		for (int j = i + 1; j <= degree; j++)
 		{
 			float ratio = x_transpose_x[i][j] / x_transpose_x[i][i];
+
 			for (int k = i; k <= degree; k++)
 			{
 				x_transpose_x[j][k] -= ratio * x_transpose_x[i][k];
 			}
+
 			x_transpose_y[j] -= ratio * x_transpose_y[i];
 		}
 	}
@@ -127,22 +139,27 @@ float polynomial_regression_train_and_test(float data[SAMPLE_SIZE][DATASET_FEATU
 	for (int i = degree; i >= 0; i--)
 	{
 		float sum = 0;
+
 		for (int j = i + 1; j <= degree; j++)
 		{
 			sum += x_transpose_x[i][j] * coefficients[j];
 		}
+
 		coefficients[i] = (x_transpose_y[i] - sum) / x_transpose_x[i][i];
 	}
 
 	// Calculate RMSE on training data
 	float rmse = 0;
+
 	for (int i = 0; i < sample_size; i++)
 	{
 		float y_pred = 0;
+
 		for (int j = 0; j <= degree; j++)
 		{
 			y_pred += coefficients[j] * pow(data[i][0], j);
 		}
+
 		rmse += pow(y_pred - data[i][feature_size - 1], 2);
 	}
 
@@ -153,13 +170,16 @@ float polynomial_regression_train_and_test(float data[SAMPLE_SIZE][DATASET_FEATU
 	{
 		free(x[i]);
 	}
+
 	free(x);
 	free(y);
+
 	for (int i = 0; i <= degree; i++)
 	{
 		free(x_transpose[i]);
 		free(x_transpose_x[i]);
 	}
+	
 	free(x_transpose);
 	free(x_transpose_x);
 	free(x_transpose_y);
